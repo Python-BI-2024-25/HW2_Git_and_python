b14f2e07bc88265444f127ad23307070bbcf2d0a
Al.i.lavrov@yandex.ru <Al.i.lavrov@yandex.ru>
2024-09-17 21:11:57 +0300

    Add main() function

---
+++ HW2_Lavrov/calculator.py
@@ -0,0 +1,15 @@
+def main():
+    a, operator, b =  input().split()
+    
+    #Переводим строки в соответствующий тип числовых данных
+    a = float(a) if "." in a else int(a)
+    b = float(b) if "." in b else int(b)
+    
+    #Отправляем числа нужной функции на вычисление 
+    if operator == "+": print(addition(a, b))
+    elif operator == "-": print(subtraction(a, b))
+    elif operator == "*": print(multiplication(a, b))
+    elif operator == "/" and b != 0: print(division(a,b))
+    elif operator == "/" and b == 0: print("Деление на ноль!")
+
+main()
