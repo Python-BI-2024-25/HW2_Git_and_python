a68828a03c3c712dc0b32ad0a13e5e43ac1d78bd
Anna Ponomareva <annaponomareva@Annas-MacBook-Pro.local>
2024-09-17 21:50:36 +0300

    Add main def and body of function

--- HW2_Ponomareva/calculator.py
+++ HW2_Ponomareva/calculator.py
@@ -1,1 +1,23 @@
+#  
+def main():
+   #  
+    list_action = input('  ').split()
 
+    # 
+    if list_action[1] in ['*', '/', '+', '-']:
+        operator = list_action[1]
+        num1 = float(list_action[0])
+        num2 = float(list_action[2])
+        if operator == '+':
+            print(f"{num1} + {num2} = {add(num1, num2)}")
+        elif operator == '-':
+            print(f"{num1} - {num2} = {subtract(num1, num2)}")
+        elif operator == '*':
+            print(f"{num1} * {num2} = {multiply(num1, num2)}")
+        elif operator == '/':
+            print(f"{num1} / {num2} = {divide(num1, num2)}")
+    else:
+        print("   !")            
+                
+
+main()
