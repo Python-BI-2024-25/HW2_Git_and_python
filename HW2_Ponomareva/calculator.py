717efdb6ad267f7c8be7d614f55d8d196ee4f0fc
Nikita Galynin <nikitagalynin@mail.ru>
2024-09-18 20:41:23 +0300

    Add division 2nd times

--- HW2_Ponomareva/calculator.py
+++ HW2_Ponomareva/calculator.py
@@ -2,6 +2,13 @@
 
 def multiply(x, y):
     return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Делить на ноль нельзя!"
+
 def main():
    #  
     list_action = input('Введите выражение: ').split()
