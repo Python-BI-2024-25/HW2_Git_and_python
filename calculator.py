145e0202de69eaebf605ed5b082de131c1c7c2f2
avemarcus <mark.kachanovskiy@gmail.com>
2024-09-19 21:44:40 +0300

    Add subtraction by Kachanovskii

--- calculator.py
+++ calculator.py
@@ -6,3 +6,11 @@
 ## multiply()
 ## divide()
 ## parse()
+
+def subtract():
+  num1 = float(input("Введите первое число: "))
+  num2 = float(input("Введите второе число: "))
+  result = num1 - num2
+  print(f"Результат: {result}")
+if __name__ == "__main__":
+  subtract()
