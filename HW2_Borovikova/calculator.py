243355e7198a4bd6242c4dd48d7f0af7c429fee5
1 <you@example.com>
2024-09-19 19:01:34 +0300

    Creat folder with main() calculator.py

---
+++ HW2_Borovikova/calculator.py
@@ -0,0 +1,15 @@
+
+#Напишите функцию здесь
+def main():
+  built = (input()).split()
+  number1=float(built[0])
+  number2=float(built[2])
+  if built[1] == '+':
+    print(addition(number1,number2))
+  elif built[1] == '-':
+     print(substraction(number1,number2))
+  elif built[1] == '*':
+    print(multiplication(number1,number2))
+  elif built[1] == '/':
+    print(divison(number1,number2)) 
+main()
