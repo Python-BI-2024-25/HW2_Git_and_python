e1c01afe6481ae0365c68572d6003172af54bd78
Anna Kalygina <113347533+AnnaKalygina@users.noreply.github.com>
2024-09-19 22:03:51 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,20 +1,46 @@
-## The implementation of a simple calculator
+## Implementation of a simple calculator
 
 ## main()
+def main():
+    expression = input()
+    expression = expression.split(" ")
+    
+    x = float(expression[0])
+    operator = expression[1]
+    y = float(expression[2])
+
+    if operator == '+':
+        result = add(x, y)
+    elif operator == '-':
+        result = subtract(x, y)
+    elif operator == '*':
+        result = multiply(x, y)
+    elif operator == '/':
+        result = divide(x, y)
+    else:
+        result = "Не то!"
+
+    return result
+
 ## add()
 def add(x, y):
     return x + y
+    
 ## subtract()
+def subtract(x, y):
+  return x - y
+    
 ## multiply()
 def multiply(x, y):
     return x * y
+    
 ## divide()
 def divide (x, y):
   if y == 0:
     print("На ноль делить нельзя")
   else: return(x / y)
-## parse()
+      
 
-def subtract(x, y):
-  return x - y
+if __name__ == "__main__":
+    main()
   
