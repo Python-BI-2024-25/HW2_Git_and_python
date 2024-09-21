4f8b16c1df12652e3b28df4c1028b9c58b266173
Aleksandr Lavrov <Al.i.lavrov@yandex.ru>
2024-09-21 16:11:45 +0300

    Bring the script into compliance with PEP8

--- HW2_Lavrov/calculator.py
+++ HW2_Lavrov/calculator.py
@@ -1,34 +1,40 @@
-#умножение
-def multiplication(a,b):
-   result = a * b
-   return result
+# умножение
+def multiplication(a, b):
+    result = a * b
+    return result
 
-def division(x,y):
-	return x/y
 
-    #Вычитание
+def division(x, y):
+    return x / y
+
+
+# Вычитание
 def subtraction(a, b):
     return a - b
 
-    #Сложение
-def addition(a,b):
+
+# Сложение
+def addition(a, b):
     result = a + b
     return result
 
 
 def main():
-    a, operator, b =  input().split()
-
-    #Переводим строки в соответствующий тип числовых данных
+    a, operator, b = input().split()
+    # Переводим строки в соответствующий тип числовых данных
     a = float(a) if "." in a else int(a)
     b = float(b) if "." in b else int(b)
+    # Отправляем числа нужной функции на вычисление
+    if operator == "+":
+        print(addition(a, b))
+    elif operator == "-":
+        print(subtraction(a, b))
+    elif operator == "*":
+        print(multiplication(a, b))
+    elif operator == "/" and b != 0:
+        print(division(a, b))
+    elif operator == "/" and b == 0:
+        print("Деление на ноль!")
 
-    
-    #Отправляем числа нужной функции на вычисление 
-    if operator == "+": print(addition(a, b))
-    elif operator == "-": print(subtraction(a, b))
-    elif operator == "*": print(multiplication(a, b))
-    elif operator == "/" and b != 0: print(division(a,b))
-    elif operator == "/" and b == 0: print("Деление на ноль!")
 
 main()
